dfaaaa355ae12e359e8bca70bdafd3f6f0cade46
Manuel <manuelmaterazzo@gmail.com>
2025-11-13 00:04:12 +0100

    real chat moderator implementation

--- processors/chat_moderator.py
+++ processors/chat_moderator.py
@@ -1,18 +1,28 @@
 import logging
 import pathway as pw
-from abc import ABC, abstractmethod
 
 
-class ChatModerator(ABC):
-
+class ChatModerator:
     # Define the schema for the moderation endpoint
     class ModerationSchema(pw.Schema):
         prompt: str
         masking: bool = pw.column_definition(default_value=False)
 
-    def __init__(self):
+    def __init__(self, moderator: pw.UDF):
         self.logger = logging.getLogger(__name__)
+        self.moderator = moderator
 
-    @abstractmethod
     def moderate(self, queries: pw.Table) -> pw.Table:
-        pass
+        """
+        Example handler that processes foobar queries.
+        Takes input queries and returns modified results.
+        """
+        toxicity = queries.select(
+            prompt=pw.this.prompt,
+            masking=pw.this.masking,
+            is_toxic=self.moderator(pw.this.prompt),
+        )
+        result = toxicity.select(
+            result=pw.apply(lambda x: {"toxic": x}, pw.this.is_toxic)
+        )
+        return result
